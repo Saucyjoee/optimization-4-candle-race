536c0bcac5137cfaeac7c73f1b743b7de2fb9b65
marcusbru1 <49483317+marcusbru1@users.noreply.github.com>
2025-05-24 17:18:27 +0200

    quickfix

--- src/main_tsp.py
+++ src/main_tsp.py
@@ -22,6 +22,7 @@ if __name__ == '__main__':
         #prob = tsp.Problem.from_textio(sys.stdin)
     else:
         #prob = Problem.from_textio("data/sample.txt")
+        pass
     if (prob):
         sol1 = solvers.greedy_construction_random_tie_breaking(Solution.empty_solution(prob))
         print("Greedy construction with random tie breaking: {:.2f}".format(sol1.objective_value()))
